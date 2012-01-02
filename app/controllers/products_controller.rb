1bee2bb998616deb9bfd13abba89540226467284
Stefanek <mstefanow@gmail.com>
2012-01-02 00:43:20 +0000

    last commit before going to sleep

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,26 @@
+require 'net/http'
+require 'rexml/document'
+
 class ProductsController < ApplicationController
 
   def index
-    logger.debug "PARAMS #{params.inspect}"
     @feed_urls = feed_urls
-    lang = params[:lang]
-    @feed_url = feed_urls[lang]
 
+    if (params[:lang]) then
+      lang = params[:lang]
+      @feed_data = get_feed(feed_urls[lang])
+    end
   end
 
   def get_feed(url)
-
-
+    xml_data = Net::HTTP.get_response(URI.parse(url)).body
+    doc = REXML::Document.new(xml_data)
+    doc.elements.each('rss/channel/item') do |item|
+       puts item.get_elements("title")
+       puts item.get_elements("link")
+       puts item.get_elements("g:image_link")
+       return item.get_elements("g:image_link")
+    end
   end
 
 end
