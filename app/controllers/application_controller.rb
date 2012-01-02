2f1724d69b68ae74846c49bbe1b196bbe39f4302
Stefanek <mstefanow@gmail.com>
2012-01-02 00:22:24 +0000

    removed index, updated readme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def feed_url
-      {:english => "http://www.arenaflowers.com/files/sitemaps/arenaflowers.com/google_base_feed.xml",
-  :french => "http://www.arenafleurs.fr/files/sitemaps/arenafleurs.fr/google_base_feed.xml",
-  :german => "http://www.arenablumen.de/files/sitemaps/arenablumen.de/google_base_feed.xml"}
+  def feed_urls
+  {"english" => "http://www.arenaflowers.com/files/sitemaps/arenaflowers.com/google_base_feed.xml",
+  "french" => "http://www.arenafleurs.fr/files/sitemaps/arenafleurs.fr/google_base_feed.xml",
+  "german" => "http://www.arenablumen.de/files/sitemaps/arenablumen.de/google_base_feed.xml"}
   end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,16 @@
 class ProductsController < ApplicationController
 
   def index
-    @feed_url = feed_url
+    logger.debug "PARAMS #{params.inspect}"
+    @feed_urls = feed_urls
+    lang = params[:lang]
+    @feed_url = feed_urls[lang]
+
+  end
+
+    def get_feed(url)
+
+
   end
 
 end
